36dc193aed49259edfc26fa3033f2ad5f392197f
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2025-01-03 12:05:11 -0800

    include forrmating and demo namespaces

--- demos/applications/tla2528_input_pin.cpp
+++ demos/applications/tla2528_input_pin.cpp
@@ -1,18 +1,18 @@
 #include <array>
+
+#include <libhal-expander/tla2528.hpp>
+#include <libhal-expander/tla2528_adapters.hpp>
 #include <libhal-util/bit.hpp>
 #include <libhal-util/i2c.hpp>
 #include <libhal-util/serial.hpp>
 #include <libhal-util/steady_clock.hpp>
 #include <libhal/input_pin.hpp>
 #include <libhal/units.hpp>
-#include <libhal-expander/tla2528.hpp>
-#include <libhal-expander/tla2528_adapters.hpp>
+
 #include <resource_list.hpp>
 
 using namespace hal::literals;
 using namespace std::chrono_literals;
-
-namespace sjsu::drivers {
 
 void application(resource_list& p_map)
 {
@@ -20,8 +20,9 @@ void application(resource_list& p_map)
   auto& i2c = *p_map.i2c.value();
   auto& steady_clock = *p_map.clock.value();
   auto gpi_expander = hal::expander::tla2528(i2c);
-  constexpr hal::input_pin::settings input_pin_config = { .resistor =
-                                                  hal::pin_resistor::none };
+  constexpr hal::input_pin::settings input_pin_config = {
+    .resistor = hal::pin_resistor::none
+  };
   std::array<hal::expander::tla2528_input_pin, 8> gpis{
     make_input_pin(gpi_expander, 0, input_pin_config),
     make_input_pin(gpi_expander, 1, input_pin_config),
@@ -41,4 +42,3 @@ void application(resource_list& p_map)
     hal::delay(steady_clock, 500ms);
   }
 }
-}  // namespace sjsu::drivers
